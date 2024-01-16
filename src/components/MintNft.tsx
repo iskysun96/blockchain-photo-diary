20dbe67194fcec063800dc5796de66ce803daa3b
iskysun96 <iskysun96@gmail.com>
2024-01-15 17:40:37 -0800

    using react-dropzone library for drag & drop image feature

--- src/components/MintNft.tsx
+++ src/components/MintNft.tsx
@@ -18,66 +18,67 @@ Minting
   = NFT name should be the emotion of the day + date (e.g. "happy 2021-09-01")
 */
 
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
 
 const MintNft = () => {
-  const [nftImage, setNftImage] = useState<File>()
   const [nftImageUrl, setNftImageUrl] = useState<string>('')
   const [nftName, setNftName] = useState<string>('')
-
-  // store user nft image file upload to CharityFormData
-  const handleFileUpload = (e: { target: { files: FileList | null } }) => {
-    const file = e.target.files?.[0]
-    if (file) {
-      setNftImage(file)
-      const reader = new FileReader()
-      reader.onload = () => {
-        setNftImageUrl(reader.result as string)
-      }
-      reader.readAsDataURL(file)
+  const onDrop = useCallback((acceptedFiles: FileList) => {
+    // setNFTImageUrl to preview image
+    const reader = new FileReader()
+    reader.onload = () => {
+      setNftImageUrl(reader.result as string)
     }
-  }
+    reader.readAsDataURL(acceptedFiles[0])
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   const handleTextInput = (e: { target: { value: string } }) => {
     setNftName(e.target.value)
   }
 
   useEffect(() => {
-    console.log(nftImage)
     console.log(nftImageUrl)
-  }, [nftImage, nftImageUrl])
+  }, [nftImageUrl])
 
   return (
     <div className="max-w-xl mt-4 mb-4">
-      {nftImage ? (
+      {nftImageUrl ? (
         <div className="flex flex-col">
-          <img src={nftImageUrl} alt="Fundraiser" />
+          <img src={nftImageUrl} />
           <input type="text" className="rounded mt-2 mb-2" placeholder="Enter NFT Name" onChange={handleTextInput} />
           <button>Create Photo Diary</button>
         </div>
       ) : (
         <label className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
-          <span className="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 text-gray-600"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-              />
-            </svg>
-            <span className="font-medium text-gray-600">
-              Drop files to Attach, or
-              <span className="text-blue-600 underline"> browse</span>
-            </span>
-          </span>
-          <input type="file" name="file_upload" className="hidden" onChange={handleFileUpload} />
+          <div {...getRootProps()} className="flex items-center">
+            {isDragActive ? (
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            ) : (
+              <span className="flex items-center space-x-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 text-gray-600"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                  />
+                </svg>
+                <span className="font-medium text-gray-600">
+                  Drop files to Attach, or
+                  <span className="text-blue-600 underline"> browse</span>
+                </span>
+              </span>
+            )}
+            <input {...getInputProps()} />
+          </div>
         </label>
       )}
     </div>
