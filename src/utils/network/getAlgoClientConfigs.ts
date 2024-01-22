82d5e0e516d2d7645cfcc075ec3ba58ce3a3647f
iskysun96 <iskysun96@gmail.com>
2024-01-21 16:47:59 -0800

    readded env imports

--- src/utils/network/getAlgoClientConfigs.ts
+++ src/utils/network/getAlgoClientConfigs.ts
@@ -4,36 +4,36 @@ export function getAlgodConfigFromViteEnvironment(): AlgoViteClientConfig {
   if (!import.meta.env.VITE_ALGOD_SERVER) {
     throw new Error('Attempt to get default algod configuration without specifying VITE_ALGOD_SERVER in the environment variables')
   }
-  return {
-    server: 'https://testnet-api.algonode.cloud',
-    port: '',
-    token: '',
-    network: 'testnet',
-  }
   // return {
-  //   server: import.meta.env.VITE_ALGOD_SERVER,
-  //   port: import.meta.env.VITE_ALGOD_PORT,
-  //   token: import.meta.env.VITE_ALGOD_TOKEN,
-  //   network: import.meta.env.VITE_ALGOD_NETWORK,
+  //   server: 'https://testnet-api.algonode.cloud',
+  //   port: '',
+  //   token: '',
+  //   network: 'testnet',
   // }
+  return {
+    server: import.meta.env.VITE_ALGOD_SERVER,
+    port: import.meta.env.VITE_ALGOD_PORT,
+    token: import.meta.env.VITE_ALGOD_TOKEN,
+    network: import.meta.env.VITE_ALGOD_NETWORK,
+  }
 }
 
 export function getIndexerConfigFromViteEnvironment(): AlgoViteClientConfig {
   if (!import.meta.env.VITE_INDEXER_SERVER) {
     throw new Error('Attempt to get default algod configuration without specifying VITE_INDEXER_SERVER in the environment variables')
   }
-  return {
-    server: 'https://testnet-idx.algonode.cloud',
-    port: '',
-    token: '',
-    network: 'testnet',
-  }
   // return {
-  //   server: import.meta.env.VITE_INDEXER_SERVER,
-  //   port: import.meta.env.VITE_INDEXER_PORT,
-  //   token: import.meta.env.VITE_INDEXER_TOKEN,
-  //   network: import.meta.env.VITE_ALGOD_NETWORK,
+  //   server: 'https://testnet-idx.algonode.cloud',
+  //   port: '',
+  //   token: '',
+  //   network: 'testnet',
   // }
+  return {
+    server: import.meta.env.VITE_INDEXER_SERVER,
+    port: import.meta.env.VITE_INDEXER_PORT,
+    token: import.meta.env.VITE_INDEXER_TOKEN,
+    network: import.meta.env.VITE_ALGOD_NETWORK,
+  }
 }
 
 export function getKmdConfigFromViteEnvironment(): AlgoViteKMDConfig {
